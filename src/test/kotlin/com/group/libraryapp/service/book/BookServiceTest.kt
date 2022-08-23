601f9f1b01f7ff9fed8d75f75a70231c748a4aff
hyun6ik <fkanwm@naver.com>
2022-08-23 19:15:04 +0900

    feat : 책 관련 기능 테스트 작성하기

---
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -0,0 +1,90 @@
+package com.group.libraryapp.service.book
+
+import com.group.libraryapp.domain.book.Book
+import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.dto.book.request.BookLoanRequest
+import com.group.libraryapp.dto.book.request.BookRequest
+import com.group.libraryapp.dto.book.request.BookReturnRequest
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceTest @Autowired constructor(
+    private val bookService: BookService,
+    private val bookRepository: BookRepository,
+    private val userRepository: UserRepository,
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+) {
+
+    @AfterEach
+    fun clear() {
+        bookRepository.deleteAll()
+        userRepository.deleteAll()
+    }
+
+    @Test
+    fun saveBookTest() {
+        //given
+        val request = BookRequest("이펙티브 코틀린")
+        //when
+        bookService.saveBook(request)
+        //then
+        val results = bookRepository.findAll()
+        assertThat(results[0].name).isEqualTo("이펙티브 코틀린")
+    }
+
+    @Test
+    fun loanBookTest() {
+        //given
+        bookRepository.save(Book("이펙티브 코틀린"))
+        val savedUser = userRepository.save(User("현식", null))
+        val request = BookLoanRequest("현식", "이펙티브 코틀린")
+        //when
+        bookService.loanBook(request)
+        //then
+        val results = userLoanHistoryRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].bookName).isEqualTo("이펙티브 코틀린")
+        assertThat(results[0].user.id).isEqualTo(savedUser.id)
+        assertThat(results[0].user.name).isEqualTo(savedUser.name)
+        assertThat(results[0].isReturn).isFalse
+    }
+
+    @Test
+    fun loanBookExceptionTest() {
+        //given
+        val savedBook = bookRepository.save(Book("이펙티브 코틀린"))
+        val savedUser = userRepository.save(User("현식", null))
+        userLoanHistoryRepository.save(UserLoanHistory(savedUser, savedBook.name, false))
+        val request = BookLoanRequest("현식", "이펙티브 코틀린")
+        //when && then
+        assertThrows<IllegalArgumentException> {
+            bookService.loanBook(request)
+        }.apply {
+            assertThat(message).isEqualTo("진작 대출되어 있는 책입니다")
+        }
+    }
+    
+    @Test
+    fun returnBookTest() {
+        //given
+        val savedBook = bookRepository.save(Book("이펙티브 코틀린"))
+        val savedUser = userRepository.save(User("현식", null))
+        userLoanHistoryRepository.save(UserLoanHistory(savedUser, savedBook.name, false))
+        val request = BookReturnRequest("현식", "이펙티브 코틀린")
+        //when
+        bookService.returnBook(request)
+        //then
+        val results = userLoanHistoryRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].isReturn).isTrue
+    }
+}
